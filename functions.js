199741058d79b013e05eecaa8cb47a84ac7d54cb
alomaryam <alomaryam@gmail.com>
2021-02-03 19:38:08 +0300

    task 5 done

--- functions.js
+++ functions.js
@@ -9,8 +9,8 @@ greet("Maryam");
 let isOdd = (x) => {
   if (x%2 == 1){
     return (true);
-  } 
-  else {return (false);
+  }{ 
+    return (false);
   }
 };
 console.log(isOdd(7));
@@ -20,8 +20,10 @@ console.log(isOdd(10));
 
 let oddsSmallerThan = (y) => {
   if (y%2 == 1){
-    return y = ((y-1)/2);
-}
+    return (y-1)/2;
+  }{
+    return y/2;
+  };
 };
 
 console.log(oddsSmallerThan(7));
@@ -32,9 +34,8 @@ console.log(oddsSmallerThan(15));
 let squareOrDouble = (z) => {
   if (z%2 == 1){
     return z = z*z
-  }
-  else 
-  { return z = z+z;
+  }{
+  return z = z+z;
 }
 };
 console.log(squareOrDouble(16));
